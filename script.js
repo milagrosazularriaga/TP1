282200e37014303074712123ea7af58fb44f8cfa
Mila <milagros.arriaga@hotmail.com>
2025-03-28 22:55:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("changeColorButton");
-
-    button.addEventListener("click", function() {
-        document.body.style.backgroundColor = 
-            document.body.style.backgroundColor === "lightgray" ? "#f8f8f8" : "lightgray";
-    });
-});
+function changeColor() {
+    document.body.style.backgroundColor = 
+        document.body.style.backgroundColor === "lightgrey" ? "white" : "lightgrey";
+}
